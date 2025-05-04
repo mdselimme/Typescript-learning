2c796ceb0d5dcd600814e3e77fb1df3071e6537f
mdselimme <mdselimdev@gmail.com>
2025-05-04 23:33:22 +0600

    add generics with function

--- 2.Number-2-Lecture/dist/generics_function.js
+++ 2.Number-2-Lecture/dist/generics_function.js
@@ -7,7 +7,13 @@ const createAnArray = (params) => {
 const createAnArrayGenerics = (params) => {
     return [params];
 };
+const createAnArrayGenericsTuple = (params, params2) => {
+    return [params, params2];
+};
 const res = createAnArray("Hello");
 const res2 = createAnArrayGenerics("Hello");
 const res3 = createAnArrayGenerics(true);
 const res4 = createAnArrayGenerics({ id: 125, name: "Selim" });
+const res5 = createAnArrayGenericsTuple("SELIM", 56);
+const res6 = createAnArrayGenericsTuple("SELIM", { name: "Hello", isAdmin: true });
+console.log(res, res2, res3, res4, res5, res6);
--- 2.Number-2-Lecture/src/generics_function.ts
+++ 2.Number-2-Lecture/src/generics_function.ts
@@ -6,24 +6,25 @@ const createAnArray = (params: string): string[] => {
     return [params]
 }
 
-const createAnArrayGenerics = <T>(params: T): T[] =>{
+const createAnArrayGenerics = <T>(params: T): T[] => {
     return [params]
 }
-const createAnArrayGenericsTuple = <T, X>(params: T, params2: X): [T, X]  =>{
+const createAnArrayGenericsTuple = <T, X>(params: T, params2: X): [T, X] => {
     return [params, params2]
 }
 
 interface User {
-    id:number,
+    id: number,
     name: string
 }
 
 const res = createAnArray("Hello")
 const res2 = createAnArrayGenerics<string>("Hello")
 const res3 = createAnArrayGenerics<boolean>(true)
-const res4 = createAnArrayGenerics<User>({id:125,name:"Selim"})
-const res5 = createAnArrayGenericsTuple("SELIM", 56)
+const res4 = createAnArrayGenerics<User>({ id: 125, name: "Selim" })
+const res5 = createAnArrayGenericsTuple<string, number>("SELIM", 56);
+const res6 = createAnArrayGenericsTuple<string, { name: string, isAdmin: boolean }>("SELIM", { name: "Hello", isAdmin: true });
 
+console.log(res, res2, res3, res4, res5, res6);
 
-
-export {}
+export { }
